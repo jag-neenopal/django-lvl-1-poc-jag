37da9c92a59b0f898694c0bdf2dd2cbafc9bb9d5
jag-neenopal <jagan.mohan@neenopal.com>
2022-11-16 17:33:19 +0530

    Finalized blod app

--- blogs/settings.py
+++ blogs/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -117,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = [
-    BASE_DIR/"uploads"
-    ]
+
+STATIC_URL = '/static/'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
